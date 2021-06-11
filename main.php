3a20395e441f3b997e3674efb0d6ad7c14573c10
masahiro96848 <feetwcpic9436@gmail.com>
2021-06-11 13:45:15 +0900

    継承 プロパティ オーバーライド ポリモーフィズム カプセル化 アクセサメソッド　コンストラクタ

--- main.php
+++ main.php
@@ -5,27 +5,26 @@
 // ファイルのロード
 require_once('./classes/Human.php');
 require_once('./classes/Enemy.php');
+require_once('./classes/Brave.php');
 
 // インスタンス化
-$tiida = new Human();
-$goblin = new Enemy();
+$tiida = new Brave('ティーダ');
+$goblin = new Enemy('ゴブリン');
 
-$tiida->name = 'ティーダ';
-$goblin->name = 'ゴブリン';
 
 // ターン数
 $turn = 1;
 
 // どちらかのHPが0になるまで繰り返す
-while($tiida->hitPoint > 0 && $goblin->hitPoint > 0){
+while($tiida->getHitPoint() > 0 && $goblin->getHitPoint() > 0){
   echo "*** $turn ターン目 ***\n\n"; 
 
-  echo $tiida->name . "\n";
-  echo $goblin->name . "\n";
+  // echo $tiida->name . "\n";
+  // echo $goblin->name . "\n";
 
   // 現在のHPの表示
-  echo $tiida->name . "　：　" . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
-  echo $goblin->name . "　：　" . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n";
+  echo $tiida->getName() . "　：　" . $tiida->getHitPoint() . "/" . $tiida::MAX_HITPOINT . "\n";
+  echo $goblin->getName() . "　：　" . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n";
   echo "\n";
 
   // 攻撃
@@ -38,5 +37,5 @@ while($tiida->hitPoint > 0 && $goblin->hitPoint > 0){
 
 }
 echo "★★★ 戦闘終了 ★★★\n\n";
-echo $tiida->name . "　：　" . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
-echo $goblin->name . "　：　" . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n\n";
+echo $tiida->getName() . "　：　" . $tiida->getHitPoint() . "/" . $tiida::MAX_HITPOINT . "\n";
+echo $goblin->getName() . "　：　" . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n\n";
