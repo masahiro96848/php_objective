25946c1dac0c6b5bc26b919d16f59d059e262e71
masahiro96848 <feetwcpic9436@gmail.com>
2021-06-11 14:38:59 +0900

    味方の攻撃　敵の攻撃をforeachを使って簡素化

--- main.php
+++ main.php
@@ -27,23 +27,55 @@ $enemies[] = new Enemy('モルボル', 30);
 // ターン数
 $turn = 1;
 
+$isFinishFlg = false;
+
 // どちらかのHPが0になるまで繰り返す
-while($tiida->getHitPoint() > 0 && $goblin->getHitPoint() > 0){
+while(!$isFinishFlg){
   echo "*** $turn ターン目 ***\n\n"; 
 
   // echo $tiida->name . "\n";
   // echo $goblin->name . "\n";
 
   // 現在のHPの表示
-  echo $tiida->getName() . "　：　" . $tiida->getHitPoint() . "/" . $tiida::MAX_HITPOINT . "\n";
-  echo $goblin->getName() . "　：　" . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n";
+  foreach($members as $member) {
+    echo $member->getName() . "　：　" . $member->getHitPoint() . "/" . $member::MAX_HITPOINT . "\n";
+  }
+  echo "\n";
+  foreach($enemies as $enemy) {
+    echo $enemy->getName() . "　：　" . $enemy->getHitPoint() . "/" . $enemy::MAX_HITPOINT . "\n";
+  }
   echo "\n";
 
-  // 攻撃
-  $tiida->doAttack($goblin);
+  // echo $goblin->getName() . "　：　" . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n";
+
+  // 味方の攻撃
+  foreach($members as $member) {
+    $enemyIndex = rand(0, count($enemies)-1 );  // 添字は0から始まるので、　-1する
+    $enemy = $enemies[$enemyIndex];
+    // 白魔導士の場合、味方のオブジェクトも渡す
+    if(get_class($member) == "WhiteMage") {
+      $member->doAttackWhiteMage($enemy, $member);
+    }else {
+      $member->doAttack($enemy);
+    }
+    echo "\n";
+  }
   echo "\n";
-  $goblin->doAttack($tiida);
-  echo "\n";
+
+
+  // 敵の攻撃
+  foreach ($enemies as $enemy) {
+      $memberIndex = rand(0, count($members) - 1); // 添字は0から始まるので、-1する
+      $member = $members[$memberIndex];
+      $enemy->doAttack($member);
+      echo "\n";
+  }
+    echo "\n";
+
+  // $tiida->doAttack($goblin);
+  // echo "\n";
+  // $goblin->doAttack($tiida);
+  // echo "\n";
 
   $turn++;
 
